13b47969a4313a182155ceb929fe34f473ae4c11
zahrawjafari <zahra.jafari8112@gmail.com>
2026-07-17 20:28:39 +0330

    z

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+import { createIcons, icons } from "lucide";
+
+createIcons({ icons });
+const password =
+document.getElementById("password");
+const copy =
+document.getElementById("copy");
+const span =
+document.getElementById("span");
+const Uppercase = document.getElementById("Uppercase");
+const Lowercase = document.getElementById("Lowercase");
+const Numbers =
+document.getElementById("Numbers");
+const Symbols = document.getElementById("Symbols");
+const bar5 = document.getElementById("bar5");
