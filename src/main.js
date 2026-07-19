ff0acea8d6e1e0cf7ce7afcd9fccf7f7141756a1
zahrawjafari <zahra.jafari8112@gmail.com>
2026-07-19 18:42:41 +0330

    z

--- src/main.js
+++ src/main.js
@@ -2,15 +2,49 @@ import "./style.css";
 import { createIcons, icons } from "lucide";
 
 createIcons({ icons });
-const password =
-document.getElementById("password");
-const copy =
-document.getElementById("copy");
-const span =
-document.getElementById("span");
+const password = document.getElementById("password");
+const copy = document.getElementById("copy");
+const span = document.getElementById("span");
 const Uppercase = document.getElementById("Uppercase");
 const Lowercase = document.getElementById("Lowercase");
-const Numbers =
-document.getElementById("Numbers");
+const Numbers = document.getElementById("Numbers");
 const Symbols = document.getElementById("Symbols");
 const bar5 = document.getElementById("bar5");
+const range = document.getElementById("range");
+function updateRange() {
+  span.textContent = range.value;
+
+  const percent = ((range.value - range.min) / (range.max - range.min)) * 100;
+
+  range.style.background = `linear-gradient(
+    to right,
+    #A4FFAF ${percent}%,
+    #18171F ${percent}%
+  )`;
+}
+updateRange();
+range.addEventListener("input", updateRange);
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*()_+-={}[]<>?";
+let characters = "";
+if (upper.checked) {
+  characters += uppercase;
+}
+if (lower.checked) {
+  characters += lowercase;
+}
+if (number.checked) {
+  characters += numbers;
+}
+if (symbol.checked) {
+  characters += symbols;
+}
+copy.addEventListener("click", () => {});
+navigator.clipboard.writeText(password.value);
+alert("Password Copied!");
+copy.addEventListener("click", () => {
+  navigator.clipboard.writeText(password.value);
+  alert("Password Copied!");
+});
