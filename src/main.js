fdc2d3f3999dfd33ed2eda459c0e0147aeecc8f6
zahrawjafari <zahra.jafari8112@gmail.com>
2026-07-28 20:32:56 +0330

    z

--- src/main.js
+++ src/main.js
@@ -3,128 +3,71 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 const codeGenerate = document.querySelector("#codeGenerate");
 const copyBtn = document.querySelector("#copyBtn");
 const range = document.querySelector("#range");
-const lengthNumber = document.querySelector("#span");
-const generateBtn = document.querySelector("#generateBtn");
+const span = document.querySelector("#span");
 const uppercase = document.querySelector("#uppercase");
 const lowercase = document.querySelector("#lowercase");
 const numbers = document.querySelector("#numbers");
 const symbols = document.querySelector("#symbols");
+const generateBtn = document.querySelector("#generateBtn");
 const status = document.querySelector("#status");
-const bars = [
-  document.querySelector("#bar1"),
-  document.querySelector("#bar2"),
-  document.querySelector("#bar3"),
-  document.querySelector("#bar4"),
-];
-const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerChars = "abcdefghijklmnopqrstuvwxyz";
-const numberChars = "0123456789";
-const symbolChars = "!@#$%^&*";
+const bar1 = document.querySelector("#bar1");
+const bar2 = document.querySelector("#bar2");
+const bar3 = document.querySelector("#bar3");
+const bar4 = document.querySelector("#bar4");
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numberLetters = "0123456789";
+const symbolLetters = "!@#$%^&*";
 range.addEventListener("input", () => {
-  lengthNumber.textContent = range.value;
-
-  let percent = ((range.value - range.min) / (range.max - range.min)) * 100;
-
+  span.textContent = range.value;
+  let value = ((range.value - range.min) / (range.max - range.min)) * 100;
   range.style.background = `
-  linear-gradient(
+    linear-gradient(
     to right,
-    #A4FFAF ${percent}%,
-    #18171F ${percent}%
-  )
-  `;
+    #A4FFAF ${value}%,
+    #18171F ${value}%
+    )
+    `;
 });
-
-generateBtn.addEventListener("click", () => {
-  let chars = "";
-
+generateBtn.addEventListener("click",()=>{
+  let characters = "";
   let password = "";
-
-  if (uppercase.checked) {
-    chars += upperChars;
-
-    password += randomChar(upperChars);
+  if(uppercase.checked){
+    characters += upperLetters;
   }
-
-  if (lowercase.checked) {
-    chars += lowerChars;
-
-    password += randomChar(lowerChars);
+  if(lowercase.checked){
+    characters += lowerLetters;
   }
-
-  if (numbers.checked) {
-    chars += numberChars;
-
-    password += randomChar(numberChars);
+  if(numbers.checked){
+    characters += numberLetters;
   }
-
-  if (symbols.checked) {
-    chars += symbolChars;
-
-    password += randomChar(symbolChars);
+  if(symbols.checked){
+    characters += symbolLetters;
   }
-  if (chars === "") {
-    alert("Select option");
+  if(characters === ""){
+    alert("Please select options");
     return;
   }
-  while (password.length < range.value) {
-    password += randomChar(chars);
+  for(let i = 0; i < range.value; i++){
+    let random =
+    Math.floor(Math.random() * characters.length);
+    password += characters[random];
   }
-  password = password
-    .split("")
-    .sort(() => Math.random() - 0.5)
-    .join("");
   codeGenerate.value = password;
-  checkStrength();
 });
-function randomChar(text) {
-  return text[Math.floor(Math.random() * text.length)];
-}
-function checkStrength() {
-  let score = 0;
-  if (uppercase.checked) score++;
-  if (lowercase.checked) score++;
-  if (numbers.checked) score++;
-  if (symbols.checked) score++;
-  if (range.value >= 10) score++;
-  if (range.value >= 15) score++;
-  bars.forEach((bar) => {
-    if (bar) {
-      bar.style.background = "#18171F";
-    }
-  });
-
-  if (score <= 2) {
-    status.textContent = "TOO WEAK!";
-
-    bars[0].style.background = "#F64A4A";
-  } else if (score <= 4) {
-    status.textContent = "WEAK";
-
-    bars[0].style.background = "#FB7C58";
-
-    bars[1].style.background = "#FB7C58";
-  } else if (score <= 5) {
-    status.textContent = "MEDIUM";
-
-    bars[0].style.background = "#F8CD65";
-
-    bars[1].style.background = "#F8CD65";
-
-    bars[2].style.background = "#F8CD65";
-  } else {
-    status.textContent = "STRONG";
-
-    bars.forEach((bar) => {
-      if (bar) {
-        bar.style.background = "#A4FFAF";
-      }
-    });
+copyBtn.addEventListener("click",()=>{
+  if(codeGenerate.value === ""){
+    return;
   }
-}
-copyBtn.addEventListener("click", () => {
-  navigator.clipboard.writeText(codeGenerate.value);
-  copyBtn.innerHTML = `<i class="fa-solid fa-check text-[#A4FFAF] text-2xl"></i>`;
-  setTimeout(() => {
-    copyBtn.innerHTML = `<i class="fa-regular fa-copy text-[#A4FFAF] text-2xl"></i>`;
-  }, 1500);
+  navigator.clipboard.writeText(
+    codeGenerate.value
+  );
+  copyBtn.innerHTML = `
+  <i class="fa-solid fa-check text-[#A4FFAF] text-2xl"></i>
+  `;
+  setTimeout(()=>{
+    copyBtn.innerHTML = `
+    <i class="fa-regular fa-copy text-[#A4FFAF] text-2xl"></i>
+    `;
+  },1500);
 });
